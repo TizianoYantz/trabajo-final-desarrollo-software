8406fdc668b8c1ad660fd79974ba29b1c70cd475
Tiziano <tiziano.yantz@gmail.com>
2026-06-14 21:34:18 -0300

    archivo crear db

--- crear_db.py
+++ crear_db.py
@@ -3,6 +3,9 @@ import sqlite3
 conn = sqlite3.connect("inventario.db")
 cursor = conn.cursor()
 
+# =========================
+# PRODUCTOS
+# =========================
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS productos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,6 +14,29 @@ CREATE TABLE IF NOT EXISTS productos (
     precio_unitario REAL NOT NULL,
     cantidad INTEGER NOT NULL,
     stock_minimo INTEGER NOT NULL
+)
+""")
+
+# =========================
+# MOVIMIENTOS
+# =========================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS movimientos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL,
+    fecha TEXT DEFAULT CURRENT_TIMESTAMP
+)
+""")
+
+# =========================
+# VENTAS
+# =========================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ventas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    producto_id INTEGER NOT NULL,
+    cantidad INTEGER NOT NULL,
+    fecha TEXT DEFAULT CURRENT_TIMESTAMP
 )
 """)
 
