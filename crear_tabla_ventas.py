17b3aecbf34208c06c59c52bd4810fabf13fb7a2
Tiziano <tiziano.yantz@gmail.com>
2026-06-14 21:26:23 -0300

    error corregido categorias

--- crear_tabla_ventas.py
+++ crear_tabla_ventas.py
@@ -4,6 +4,25 @@ conn = sqlite3.connect("inventario.db")
 cursor = conn.cursor()
 
 cursor.execute("""
+CREATE TABLE IF NOT EXISTS productos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT,
+    categoria TEXT,
+    precio_unitario REAL,
+    cantidad INTEGER,
+    stock_minimo INTEGER
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS movimientos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT,
+    fecha TEXT DEFAULT CURRENT_TIMESTAMP
+)
+""")
+
+cursor.execute("""
 CREATE TABLE IF NOT EXISTS ventas (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     producto_id INTEGER,
